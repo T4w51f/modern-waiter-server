abe0784db209c9935524556ab0a3816d20d34954
Tawsif <tawsifhasan1998@gmail.com>
2021-01-07 20:52:34 -0800

    fixed db

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ require('dotenv').config()
 const { Pool } = require('pg')
 const isProduction = process.env.NODE_ENV === 'production'
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+const connectionString = 'postgresql://qfdvdcqxzsyiql:841b4b524ee59694e981007fac92eff46183c42dd3fa7e3f12fae03485951f92@ec2-54-147-126-202.compute-1.amazonaws.com:5432/d7s1co5ivt82qu'
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
   ssl: isProduction,
 })
 
-module.exports = pool
+module.exports = { pool }
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -11,14 +11,15 @@ function getRestaurant(req, res){
     console.log("GET /restaurants/{{id}}")
 
     let id = parseInt(req.params.id,10)
+
     if (isNaN(id)){
         res.status(400).send("Invalid id type, must be an integer")
-        // return
+        return
     }
 
-    pool.query('SELECT * FROM restaurant WHERE id = ?', [id], function(err, result){
-        res.status(200).json(result)
-        // return
+    pool.query("SELECT * FROM restaurant WHERE id = ?", [id], function(err, result){
+        res.status(200).send(result)
+        return
     })
 }
 
