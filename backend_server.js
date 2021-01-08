f691ad7b5a90712fe463854653d8eb76bf799a3e
Tawsif <tawsifhasan1998@gmail.com>
2021-01-07 20:14:10 -0800

    uncommented routes

--- backend_server.js
+++ backend_server.js
@@ -24,45 +24,45 @@ app.get('/_health', (req, res) => {
     res.status(200).send('ok')
   })
 
-// // Routes for items
-// app.get("/items/:restaurantId", items.getMenu)
-// app.post("/items", items.addToMenu)
+// Routes for items
+app.get("/items/:restaurantId", items.getMenu)
+app.post("/items", items.addToMenu)
 
-// // Routes for ordered items
-// app.get("/ordered-items/:orderId", ordered_items.getOrderedItems)
-// app.post("/ordered-items", ordered_items.addOrderedItems)
-// app.put("/ordered-items/paid", ordered_items.updateOrderedItemPaidStatus)
-// app.put("/ordered-items/selected", ordered_items.updateSelectedStatus)
+// Routes for ordered items
+app.get("/ordered-items/:orderId", ordered_items.getOrderedItems)
+app.post("/ordered-items", ordered_items.addOrderedItems)
+app.put("/ordered-items/paid", ordered_items.updateOrderedItemPaidStatus)
+app.put("/ordered-items/selected", ordered_items.updateSelectedStatus)
 
-// // Routes for orders
-// app.post("/orders", orders.createOrder)
-// app.get("/orders/user/:users_id", orders.getUserOrder)
-// app.get("/orders/table/:tables_id", orders.getTableOrder)
-// app.put("/orders/session", orders.updateOrderSessionStatus)
-// app.put("/orders/paid", orders.updateOrderPaidStatus)
+// Routes for orders
+app.post("/orders", orders.createOrder)
+app.get("/orders/user/:users_id", orders.getUserOrder)
+app.get("/orders/table/:tables_id", orders.getTableOrder)
+app.put("/orders/session", orders.updateOrderSessionStatus)
+app.put("/orders/paid", orders.updateOrderPaidStatus)
 
-// // Routes for Stripe (external API)
-// app.get('/key', payment.getStripeKey)
-// app.post('/pay', payment.createStripePayment)
+// Routes for Stripe (external API)
+app.get('/key', payment.getStripeKey)
+app.post('/pay', payment.createStripePayment)
 
-// // Routes for item recommendation
-// app.get("/recommendation/:userId/:restaurantId", recommendation.getItemRecommendation) 
-// app.get("/recommendation/keywords", recommendation.getKeyWords) 
+// Routes for item recommendation
+app.get("/recommendation/:userId/:restaurantId", recommendation.getItemRecommendation) 
+app.get("/recommendation/keywords", recommendation.getKeyWords) 
 
-// // Routes for restaurant
-// app.get("/restaurants/:id", restaurants.getRestaurant)
-// app.post("/restaurants", restaurants.addRestaurant)
+// Routes for restaurant
+app.get("/restaurants/:id", restaurants.getRestaurant)
+app.post("/restaurants", restaurants.addRestaurant)
 
-// // Routes for table
-// app.get("/tables/:id", tables.getTable)
-// app.post("/tables", tables.addTable)
+// Routes for table
+app.get("/tables/:id", tables.getTable)
+app.post("/tables", tables.addTable)
 
-// // Routes for user
-// app.post("/users", users.addUser)
-// app.get("/users/:id", users.getUserById)
-// app.get("/users/google/:googleId", users.getUserByGoogleId)
-// app.get("/users/preferences/:id", users.getUserPreferences)
-// app.put("/users/preferences", users.updateUserPreferences)
+// Routes for user
+app.post("/users", users.addUser)
+app.get("/users/:id", users.getUserById)
+app.get("/users/google/:googleId", users.getUserByGoogleId)
+app.get("/users/preferences/:id", users.getUserPreferences)
+app.put("/users/preferences", users.updateUserPreferences)
 
 /**
  * HTTP POST request to register token for
